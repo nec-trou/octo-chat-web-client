c488078ef9ef72fe39a9a9527f14ce2bb1496c4d
nec-trou <darya.bilyalova@gmail.com>
2019-12-26 14:44:38 +0500

    fix: get rid of emply disconnected messages

--- server.js
+++ server.js
@@ -33,7 +33,10 @@ io.on('connection', client => {
     users--
     console.log(`Client ${id} disconnected. Users left: ${users}`)
     const nickname = usersOnline.get(id)
-    io.emit('message', { nickname, msg: 'disconnected', type: 'info' })
+    usersOnline.delete(id)
+    if (nickname != '') {
+      io.emit('message', { nickname, msg: 'disconnected', type: 'info' })
+    }
   })
 })
 
